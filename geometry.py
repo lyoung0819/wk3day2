f1ecba76203387aae77c00fbe4967ad7cdee2945
lyoung0819 <159396936+lyoung0819@users.noreply.github.com>
2024-03-06 10:41:57 -0600

    fixed

--- geometry.py
+++ geometry.py
@@ -1,11 +1,11 @@
-from math import pi
+from math import pi, sqrt
 
 def area(rad):
     answer = pi*rad**2
     return answer
 
 def hypotenuse(x, y):
-    answer = (x*x + y*y)**0.5
+    answer = sqrt(x**2 + y**2)
     return answer
 
 
